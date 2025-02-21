d1458eee85a1d013c7fb2745e3d3ef79c8d5960d
dbsrkdud <yungayoung63@gmail.com>
2025-02-21 09:58:49 +0900

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,23 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header.jsx";
 import NavBar from "./components/Navbar.jsx"
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/home/Home.jsx";
+import Order from "./pages/order/Order.jsx";
+import Cart from "./pages/cart/Cart.jsx";
+import MyPage from "./pages/myPage/MyPage.jsx";
 
 
 function App() {
 
     return (
         <div className="App">
+            <Routes>
+                <Route path="home" element={<Home/>}></Route>
+                <Route path="order" element={<Order/>}></Route>
+                <Route path="cart" element={<Cart/>}></Route>
+                <Route path="myPage" element={<MyPage/>}></Route>
+            </Routes>
             <Header />
             <NavBar />
         </div>
